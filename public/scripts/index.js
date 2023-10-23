3cbe1b837e36940438f204f5a0ecd42fef33edbc
Prabh <sonukaur68@gmail.com>
2023-10-23 05:43:19 -0600

    create a index.js file to create elements for categories

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,37 +8,37 @@
 
 $(() => {
   console.log("I am in dishes");
-    getAllDishes()
-      .then(function(dishes) {
-        console.log("dishes are", dishes);
-        renderDishes(dishes);
-      });
+  getAllDishes()
+    .then(function(dishes) {
+      console.log("dishes are", dishes);
+      renderDishes(dishes);
+    });
 
 
-    function renderDishes(dishes) {
-      const $CatgoriesListing = $(`
+  function renderDishes(dishes) {
+    const $CatgoriesListing = $(`
               <article class="categories">
                   <h2>Categories</h2>
                   <div class="category_container">
                   </div>
               </article>
           `);
-      // Loop through the dishes data and create HTML elements for each dish
-      dishes.forEach(dish => {
-        const $categoryCard = $(`
+    // Loop through the dishes data and create HTML elements for each dish
+    dishes.forEach(dish => {
+      const $categoryCard = $(`
           <div class="category_cards">
               <h3>${dish.category}</h3>
               <img src="${dish.image}" alt="${dish.name} sample">
           </div>
       `);
 
-        $CatgoriesListing.find('.category_container').append($categoryCard);
-      });
+      $CatgoriesListing.find('.category_container').append($categoryCard);
+    });
 
-      // Append the categories listing to a container element on your page
-      $('.container').append($CatgoriesListing);
+    // Append the categories listing to a container element on your page
+    $('.dishCategories').append($CatgoriesListing);
 
-    }
+  }
 
-  });
+});
 
