f6f7887c58602cc83544b100fcb0909394d1ad13
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-23 10:02:21 -0600

    Modify schema to correct errors and add description to order. Add images and dishes.sql to insert orders to app.

---
+++ db/seeds/dishes.sql
@@ -0,0 +1,85 @@
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Spaghetti Bolognese',
+    12.99,
+    'images/spaghetti_bolognese.jpg',
+    1,
+    'Classic spaghetti served with a rich and hearty Bolognese sauce. A favorite Italian comfort dish.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Margherita Pizza',
+    9.99,
+    'images/margherita_pizza.jpg',
+    1,
+    'A traditional Margherita pizza with fresh tomatoes, mozzarella, basil, and olive oil on a thin crust.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Chicken Tikka Masala',
+    14.99,
+    'images/chicken_tikka_masala.jpg',
+    1,
+    'Tender chicken pieces marinated in spices and served in a creamy tomato-based sauce. Served with naan or rice.'
+);
+
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Sushi Combo',
+    18.99,
+    'images/sushi_combo.jpg',
+    1,
+    'A delightful combination of fresh sushi rolls, sashimi, and nigiri. Perfect for sushi lovers.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Caesar Salad',
+    8.99,
+    'images/caesar_salad.jpg',
+    1,
+    'A classic Caesar salad with crisp romaine lettuce, croutons, Parmesan cheese, and Caesar dressing.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Cheeseburger',
+    10.99,
+    'images/cheeseburger.jpg',
+    1,
+    'A juicy beef patty with melted cheese, lettuce, tomato, and special sauce in a soft bun.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Chocolate Cake',
+    7.99,
+    'images/chocolate_cake.jpg',
+    1,
+    'A rich and decadent chocolate cake topped with chocolate ganache. A sweet ending to your meal.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Mushroom Risotto',
+    15.99,
+    'images/mushroom_risotto.jpg',
+    1,
+    'Creamy and flavorful mushroom risotto made with Arborio rice, sautéed mushrooms, and a touch of Parmesan cheese.'
+);
+
+INSERT INTO DISHES (name, price, image, restaurant_id, description)
+VALUES (
+    'Grilled Salmon',
+    16.99,
+    'images/grilled_salmon.jpg',
+    1,
+    'Grilled to perfection, our salmon fillet is a healthy and delicious choice. Served with a lemon-dill sauce.'
+);
+
+
+
+
