5e1596a1aad533dd0c417b3457bcd1eab7abb921
Prabh <sonukaur68@gmail.com>
2023-10-23 08:21:18 -0600

    adding functionality to index.js file

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,35 +1,28 @@
-
-// $(() => {
-// //   getRestaurant().then(function(json) {
-
-// //     //Here Added to Form elements 
-// //   });
-// // });
-
 $(() => {
-  console.log("I am in dishes");
   getAllDishes()
     .then(function(dishes) {
       console.log("dishes are", dishes);
-      renderDishes(dishes);
+      renderDishesCategories(dishes);
+      renderPopularDishes(dishes);
     });
 
-
-  function renderDishes(dishes) {
+  function renderDishesCategories(dishes) {
     const $CatgoriesListing = $(`
-              <article class="categories">
-                  <h2>Categories</h2>
-                  <div class="category_container">
-                  </div>
-              </article>
-          `);
-    // Loop through the dishes data and create HTML elements for each dish
-    dishes.forEach(dish => {
+      <article class="categories">
+        <h2>Categories</h2>
+        <div class="category_container">
+        </div>
+      </article>
+    `);
+
+    // Loop through the dishes data and create HTML elements for each category
+    const uniqueCategories = Array.from(new Set(dishes.map(dish => dish.category)));
+    uniqueCategories.forEach(category => {
       const $categoryCard = $(`
-          <div class="category_cards">
-              <h3>${dish.category}</h3>
-              <img src="${dish.image}" alt="${dish.name} sample">
-          </div>
+        <div class="category_cards">
+          <h3>${category}</h3>
+          <!-- You might add some category-specific content here -->
+        </div>
       `);
 
       $CatgoriesListing.find('.category_container').append($categoryCard);
@@ -37,8 +30,32 @@ $(() => {
 
     // Append the categories listing to a container element on your page
     $('.dishCategories').append($CatgoriesListing);
-
   }
 
-});
+  function renderPopularDishes(dishes) {
+    // Select the cards container for popular dishes
+    const $popularDishesContainer = $('.popular_dishes .cards_container');
 
+    // Loop through the 'dishes' array and create popular dish cards
+    dishes.forEach(dish => {
+      const $popularDishCard = $(`
+        <div class="dish_cards">
+          <img src="${dish.image}" alt="${dish.name} descriptive text">
+          <footer>
+            <div>
+              <span>${dish.name}</span>
+              <i>Favourite icon</i>
+            </div>
+            <div>
+              <h3>$${dish.price}</h3>
+              <small><em>Delivery fee</em> $2</small>
+            </div>
+          </footer>
+        </div>
+      `);
+
+      // Append each popular dish card to the popular dishes container
+      $popularDishesContainer.append($popularDishCard);
+    });
+  }
+});
