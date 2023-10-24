83eab96b4d9674218e459091294e0d30df3d0ac6
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-24 06:34:32 -0600

    Fix restaurant.sql  description insertion

--- db/seeds/restaurant.sql
+++ db/seeds/restaurant.sql
@@ -1,6 +1,6 @@
 INSERT INTO RESTAURANT (name, description, phone_number)
 VALUES (
     "Luna's Kitchen",
-    "Luna's Kitchen, a ghost kitchen, serves up a diverse menu of meticulously crafted dishes, from classic Italian "Spaghetti Bolognese" to a delightful "Sushi Combo," all delivered to your doorstep for a memorable dining experience. Enjoy culinary excellence in the comfort of your own home with Luna's Kitchen's mouthwatering offerings.",
+    `Luna's Kitchen, a ghost kitchen, serves up a diverse menu of meticulously crafted dishes, from classic Italian "Spaghetti Bolognese" to a delightful "Sushi Combo," all delivered to your doorstep for a memorable dining experience. Enjoy culinary excellence in the comfort of your own home with Luna's Kitchen's mouthwatering offerings.`,
     1239119111
 ); 
