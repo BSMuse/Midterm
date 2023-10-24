489e891d19879107239a57535170e1bdad18212f
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-24 06:40:56 -0600

    Fix table to have serial value for ids

--- db/schema/database.sql
+++ db/schema/database.sql
@@ -1,19 +1,18 @@
-
 CREATE TABLE CLIENTS (
-    client_id INT PRIMARY KEY,
+    client_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone_number VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE RESTAURANT (
-    restaurant_id INT PRIMARY KEY,
+    restaurant_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     phone_number VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE DISHES (
-    dish_id INT PRIMARY KEY,
+    dish_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
@@ -24,7 +23,7 @@ CREATE TABLE DISHES (
 );
 
 CREATE TABLE ORDERS (
-    order_id INT PRIMARY KEY,
+    order_id SERIAL PRIMARY KEY,
     pickup_time TIME,
     order_status BOOLEAN DEFAULT FALSE,
     total_order_amount DECIMAL(10, 2),
@@ -35,7 +34,7 @@ CREATE TABLE ORDERS (
 );
 
 CREATE TABLE NOTIFICATIONS (
-    notification_id INT PRIMARY KEY,
+    notification_id SERIAL PRIMARY KEY,
     message TEXT,
     timestamp TIMESTAMP,
     client_id INT,
@@ -44,9 +43,8 @@ CREATE TABLE NOTIFICATIONS (
     FOREIGN KEY (restaurant_id) REFERENCES RESTAURANT(restaurant_id) ON DELETE CASCADE
 );
 
-
 CREATE TABLE SMSLOGS (
-    log_id INT PRIMARY KEY,
+    log_id SERIAL PRIMARY KEY,
     sender VARCHAR(50) CHECK (sender IN ('Client', 'Restaurant')),
     recipient VARCHAR(50) CHECK (recipient IN ('Client', 'Restaurant')),
     message TEXT,
