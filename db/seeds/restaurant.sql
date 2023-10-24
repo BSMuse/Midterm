41dfc8987815d1e4226e1f98d45e12ef41f80565
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-24 06:43:34 -0600

    Fix value insertion for restaurant due to use of double quotes, not single

--- db/seeds/restaurant.sql
+++ db/seeds/restaurant.sql
@@ -1,6 +1,6 @@
 INSERT INTO RESTAURANT (name, description, phone_number)
 VALUES (
-    "Luna's Kitchen",
-    "Luna's Kitchen, a ghost kitchen, serves up a diverse menu of meticulously crafted dishes.",
+    'Luna''s Kitchen',
+    'Luna''s Kitchen, a ghost kitchen, serves up a diverse menu of meticulously crafted dishes.',
     1239119111
-); 
+);
