73fd50f0957b16b09cc377dd37975d68b7a45d59
KenniCodes <kennedygosnell@outlook.com>
2023-10-26 06:34:07 -0700

    add 'addToCart.js' to hold the addToCart() then linked it to appropriate pages

--- client/scripts/cart.js
+++ client/scripts/cart.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  // Initialize cart with an empty array
-  let cart = [];
 
   function renderCartItems(cart) {
     $('.cart-container').empty();
@@ -38,36 +36,6 @@ $(document).ready(function () {
     let total = subtotal + tax;
     $('.order_total h3').text('Total: $' + total.toFixed(2));
   }
-
-  function addToCart(newItem) {
-    // Takes in dish and pushes to cart
-    cart.push(newItem);
-    // To check what is in the cart array
-    console.log(cart);
-    // AJAX post to server as JSON
-    $.ajax({
-      url: '/api/orders/add-to-cart',
-      method: 'POST',
-      data: { newItem: JSON.stringify(newItem) },
-      success: function (response) {
-        console.log("Cart updated:", response);
-      }
-    });
-  };
-
-  $(document).on('click', '.addToCart', function () {
-    // Takes data from 'this' dynamically rendered dish card and places into var
-    const dishCard = $(this).closest('.dish_cards');
-    const dishName = dishCard.find('span').text();
-    const dishPrice = parseFloat(dishCard.find('h3').text().replace('$', ''));
-    // Var added for easier readability
-    const newItem = {
-      name: dishName,
-      price: dishPrice
-    };
-
-    addToCart(newItem);
-  });
 
   $(document).on('click', '.delete-item', function () {
     const dishContainer = $(this).closest('.dish_info');
