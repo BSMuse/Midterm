2b1f8a4f0158a111c464eb789b4d69621b70e728
Prabh <sonukaur68@gmail.com>
2023-10-26 11:42:11 -0600

    resolve conflict

--- routes/orders-api.js
+++ routes/orders-api.js
@@ -1,12 +1,7 @@
 const express = require('express');
-<<<<<<< HEAD
+
 const router  = express.Router();
 const userQueries = require('../db/queries/dishes.js');
-=======
-const router = express.Router();
-const userQueries = require('../db/queries/dishes');
->>>>>>> 451c6ec3daf7a2de4248c823e306f9224a840830
-
 
 // router.use((req, res, next) => {
 //   if (!req.session.user_id) {
--- routes/users-api.js
+++ routes/users-api.js
@@ -5,12 +5,6 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-<<<<<<< HEAD
-const router = express.Router();
-const userQueries = require('../db/queries/user');
-=======
->>>>>>> 451c6ec3daf7a2de4248c823e306f9224a840830
 
 const router = express.Router();
 const userQueries = require('../db/queries/user');
@@ -26,13 +20,5 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
-  console.log('test')
-  // Set a session variable when the index page loads
-  req.session.user_id = '123'; // Replace '123' with the user's ID or any value you want to set
-
-  // Send the index.html file as a response
-  res.sendFile('index.html', { root: __dirname + '/public' });
-});  
 
 module.exports = router;
