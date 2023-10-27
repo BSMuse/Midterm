317f5f1ee8a8515e9fc04331a25537eccbbf773f
Alexander Da Costa <bsmuse@tutamail.com>
2023-10-26 22:05:02 -0600

    Adjusting text_twilio

--- client/scripts/text_twilio.js
+++ client/scripts/text_twilio.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  // Make a GET request to trigger the Twilio message sending on page load
-  $.get('/send-sms', function(data) {
-      console.log(data); // You can log the response from the server
+  $.get('/api/send-sms', function(data) {
+      console.log(data); 
   });
 });
